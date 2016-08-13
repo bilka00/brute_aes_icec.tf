a18bb34de663873393ee3e0b2c98f2eff9bf6cb6
bilka00 <bilka00@mail.ru>
2016-08-13 19:14:31 +0300

    Тип релиз

---
+++ main.py
@@ -0,0 +1,17 @@
+#from Crypto.Cipher.AES import AESCipher
+
+import base64
+import itertools
+
+data = "" #TODO: request data
+pass_list = itertools.combinations("1234567890", 4)
+
+for key in pass_list:
+    try:
+        data = base64.b64decode(data)
+        new_data = AESCipher("".join(key).decrypt(data)
+        if new_data.find("IceCTF"):
+            print "".join(key)."\n"
+            print new_data
+    except:
+        continue
