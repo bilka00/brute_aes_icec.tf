5dfc6d5866b5914d1d00e0c1d80cdc88421a5987
bilka00 <bilka00@mail.ru>
2016-08-13 19:22:37 +0300

    Тип релиз

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pass_list = itertools.combinations("1234567890", 4)
 for key in pass_list:
     try:
         data = base64.b64decode(data)
-        new_data = AESCipher("".join(key).decrypt(data))
+        new_data = AESCipher("".join(key)).decrypt(data)
         if new_data.find("IceCTF")!=-1:
             print "".join(key)+"\n"
             print new_data
