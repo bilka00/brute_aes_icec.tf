15a3ef366a510201c576bae7e46655ab6ce62e27
bilka00 <bilka00@mail.ru>
2016-08-13 19:16:44 +0300

    Тип релиз

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from Crypto.Cipher.AES import AESCipher
+from Crypto.Cipher.AES import AESCipher
 
 import base64
 import itertools
@@ -10,7 +10,7 @@ for key in pass_list:
     try:
         data = base64.b64decode(data)
         new_data = AESCipher("".join(key).decrypt(data)
-        if new_data.find("IceCTF"):
+        if new_data.find("IceCTF")!=-1:
             print "".join(key)."\n"
             print new_data
     except:
